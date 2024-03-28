af7c7df8d8a00c0e9be202818d566e0bca4973de
Precious Gartrell <precious.gartrell@allovue.com>
2024-03-28 13:53:16 -0400

    identicon up and running

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,28 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def main(input_string) do
+    input_string
+    |> hash_input()
+    |> pick_color()
   end
+
+def pick_color(image_struct) do
+  #Image_struct looks something like this 
+  #%Identicon.Image{
+    #hex: [145, 46, 200, 3, 178, 206, 73, 228, 165, 65, 6, 141, 73, 90, 181, 112]
+  #}
+
+    %Identicon.Image{hex: hex_list} = image_struct
+    [r, g, b | _] = hex_list
+
+    [r, g, b]
+end
+
+def hash_input(input_string) do
+  hex = 
+  :crypto.hash(:md5, input_string)
+  |> :binary.bin_to_list
+
+  %Identicon.Image{hex: hex}
+end
+
 end
---
+++ identicon/lib/image.ex
@@ -0,0 +1,5 @@
+defmodule Identicon.Image do
+    # We can now make use of this anywhere in the application
+    # We can only use this Identicon.Image with this hex property. Cannot add on any other properties when using it
+    defstruct hex: nil
+end
