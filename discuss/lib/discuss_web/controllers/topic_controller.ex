32e64816cf5fa55f43f623b204a893155d4b8f8e
Precious Gartrell <precious.gartrell@allovue.com>
2024-05-18 14:55:07 -0400

    fixed controller issues

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,8 +1,12 @@
 defmodule DiscussWeb.TopicController do
     use DiscussWeb, :controller
 
+    alias Discuss.Discussions.Topic
+
     def new(conn, _params) do
- 
+        Topic.changeset(%Topic{}, %{})
+
         render(conn, :new)
     end
 end
+
